5a89ee6d5ee6c7a8f6fa2e141a25ef434ba6f19f
vxxxxc <veper1234@icloud.com>
2025-06-29 00:18:56 +0800

    swift project git init

---
+++ ForgotTests/ForgotTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgotTests.swift
+//  ForgotTests
+//
+//  Created by VC on 28/6/2025.
+//
+
+import Testing
+@testable import Forgot
+
+struct ForgotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
